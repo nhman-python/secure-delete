986c113952e345a9bf04a7f246c06ae66ec9cd6c
python user <104791322+nhman-python@users.noreply.github.com>
2023-07-17 02:20:31 +0300

    Update delete_file_secure.py

--- delete_file_secure.py
+++ delete_file_secure.py
@@ -24,25 +24,30 @@ print(ascii_banner)
 
 def secure_delete(file_path, strong=1):
     if os.path.isfile(file_path):
-        print(Colors.YELLOW + '[-] Starting secure deletion of the file. Please wait a minute...' + Colors.RESET)
-        data = os.urandom(1024)
-        size = os.path.getsize(file_path)
-        num_loop = size // 1024
-        remaining_bytes = num_loop // 1024
+        q = input(Colors.YELLOW + '[?] Are you sure you want to continue? This will delete your file without an '
+                                  'option to restore it. (y/n) ' + Colors.RESET)
+        if q.lower() == 'y':
+            print(Colors.YELLOW + '[-] Starting secure deletion of the file. Please wait a minute...' + Colors.RESET)
+            data = os.urandom(1024)
+            size = os.path.getsize(file_path)
+            num_loop = size // 1024
+            remaining_bytes = num_loop // 1024
 
-        try:
-            for _ in range(strong):
-                with open(file_path, 'wb') as f:
-                    for _ in range(num_loop):
-                        f.write(data)
+            try:
+                for _ in range(strong):
+                    with open(file_path, 'wb') as f:
+                        for _ in range(num_loop):
+                            f.write(data)
 
-                    f.write(data[:remaining_bytes])
+                        f.write(data[:remaining_bytes])
 
-            os.remove(file_path)
-            print(Colors.GREEN + f'[!] The file {file_path} has been securely deleted!' + Colors.RESET)
+                os.remove(file_path)
+                print(Colors.GREEN + f'[!] The file {file_path} has been securely deleted!' + Colors.RESET)
 
-        except (PermissionError, ValueError, KeyboardInterrupt) as err:
-            print(Colors.RED + str(err) + Colors.RESET)
+            except (PermissionError, ValueError, KeyboardInterrupt) as err:
+                print(Colors.RED + str(err) + Colors.RESET)
+        else:
+            print(Colors.RED + '[x] cancel the delete' + Colors.RESET)
     else:
         print(Colors.RED + "[?] The provided path is not a file or does not exist!" + Colors.RESET)
 
@@ -56,3 +61,8 @@ def main():
                                                  'Higher values increase security but take more time.'
                                                  'recommend 4')
     args = parser.parse_args()
+    secure_delete(args.file_path, args.strong)
+
+
+if __name__ == '__main__':
+    main()
