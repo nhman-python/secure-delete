5ad1a17886b9a042777cee30d32a1342cc8b81aa
python user <104791322+nhman-python@users.noreply.github.com>
2023-07-18 13:03:42 +0300

    Update run.py

--- run.py
+++ run.py
@@ -2,27 +2,20 @@ import argparse
 import os
 from art import text2art
 
-banner = text2art('secure delete')
-
-
-# ANSI escape sequences for color
 class Colors:
+    """ANSI escape sequences for color"""
     GREEN = '\033[92m'
     RED = '\033[91m'
     YELLOW = '\033[93m'
     RESET = '\033[0m'
 
-
-ascii_banner = f'{Colors.RED}{banner}{Colors.GREEN}\t\t\t\twrite by @github.com/nhman-python{Colors.RESET}\n'
-print(ascii_banner)
-
-
 def secure_delete(file_path, strong=1):
+    """Securely deletes a file by overwriting it multiple times."""
     if os.path.isfile(file_path):
-        q = input(Colors.YELLOW + '[?] Are you sure you want to continue? This will delete your file without an '
-                                  'option to restore it. (y/n) ' + Colors.RESET)
+        q = input(f'{Colors.YELLOW}[?] Are you sure you want to continue? This will delete your file without an '
+                  f'option to restore it. (y/n) {Colors.RESET}')
         if q.lower() == 'y':
-            print(Colors.YELLOW + '[-] Starting secure deletion of the file. Please wait a minute...' + Colors.RESET)
+            print(f'{Colors.YELLOW}[-] Starting secure deletion of the file. Please wait a minute...{Colors.RESET}')
             data = os.urandom(1024)
             size = os.path.getsize(file_path)
             num_loop = size // 1024
@@ -37,24 +30,29 @@ def secure_delete(file_path, strong=1):
                         f.write(data[:remaining_bytes])
 
                 os.remove(file_path)
-                print(Colors.GREEN + f'[!] The file {file_path} has been securely deleted!' + Colors.RESET)
+                print(f'{Colors.GREEN}[!] The file {file_path} has been securely deleted!{Colors.RESET}')
 
             except (PermissionError, ValueError, KeyboardInterrupt) as err:
-                print(Colors.RED + str(err) + Colors.RESET)
+                print(f'{Colors.RED}{err}{Colors.RESET}')
         else:
-            print(Colors.RED + '[x] cancel the delete' + Colors.RESET)
+            print(f'{Colors.RED}[x] Cancel the delete{Colors.RESET}')
     else:
-        print(Colors.RED + "[?] The provided path is not a file or does not exist!" + Colors.RESET)
+        print(f'{Colors.RED}[?] The provided path is not a file or does not exist!{Colors.RESET}')
 
 
 def main():
+    """Entry point of the script"""
+    banner = text2art('secure delete')
+    ascii_banner = f'{Colors.RED}{banner}{Colors.GREEN}\t\t\t\twrite by @github.com/nhman-python{Colors.RESET}\n'
+    print(ascii_banner)
+
     parser = argparse.ArgumentParser(prog='Secure File Deletion',
                                      description='This script helps you securely delete a file '
                                                  'by overwriting it multiple times.')
     parser.add_argument('file_path', type=str, help='The path to the file that you want to delete securely.')
     parser.add_argument('strong', type=int, help='The number of overwrite passes to make on the file. '
-                                                 'Higher values increase security but take more time.'
-                                                 'recommend 4')
+                                                 'Higher values increase security but take more time. '
+                                                 'Recommend 4.')
     args = parser.parse_args()
     secure_delete(args.file_path, args.strong)
 
