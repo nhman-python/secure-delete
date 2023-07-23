952a2671820afde639d2d4f67e4722e02aaad539
python user <104791322+nhman-python@users.noreply.github.com>
2023-07-23 13:05:43 +0300

    Update secure-delete.py
    add more readability to the code and add rename before delete the file

--- secure-delete.py
+++ secure-delete.py
@@ -1,39 +1,46 @@
-"""
-will delete you're file securely to protect yor file from restore software
-"""
 import argparse
 import os
 import art
+import random
+import string
 
 GREEN = '\033[92m'
 RED = '\033[91m'
 YELLOW = '\033[93m'
 RESET = '\033[0m'
 
-
 def secure_delete(file_path: str, strong: int = 1) -> None:
-    """Securely deletes a file by overwriting it multiple times."""
+    """
+    Securely deletes a file by overwriting it multiple times and renaming it with random names.
+    """
     if os.path.isfile(file_path):
         user_input = input(f'{YELLOW}[?] Are you sure you want to continue? '
                            f'This will delete your file without '
                            f'an option to restore it. (y/n) {RESET}')
         if user_input.lower() == 'y':
-            print(f'{YELLOW}[-] Starting secure deletion of the file.'
-                  f' Please wait a minute...{RESET}')
-            data = os.urandom(1024)
-            file_size = os.path.getsize(file_path)
-            num_loops = file_size // 1024
-            remaining_bytes = num_loops % 1024
+            print(f'{YELLOW}[-] Starting secure deletion of the file. '
+                  f'Please wait a moment...{RESET}')
+            
+            # Renaming the file with random names
+            for _ in range(strong):
+                random_name = ''.join(random.choices(string.ascii_letters, k=8))
+                renamed_file_path = os.path.join(os.path.dirname(file_path), random_name)
+                os.rename(file_path, renamed_file_path)
 
+                # Overwriting the renamed file with random data
+                try:
+                    with open(renamed_file_path, 'wb') as file:
+                        file.write(os.urandom(os.path.getsize(renamed_file_path)))
+                    
+                except (PermissionError, ValueError, KeyboardInterrupt) as err:
+                    print(f'{RED}{err}{RESET}')
+
+            # Truncating the file before final deletion
             try:
-                for _ in range(strong):
-                    with open(file_path, 'wb') as file:
-                        for _ in range(num_loops):
-                            file.write(data)
+                with open(renamed_file_path, 'wb') as file:
+                    file.truncate()
 
-                        file.write(data[:remaining_bytes])
-
-                os.remove(file_path)
+                os.remove(renamed_file_path)
                 print(f'{GREEN}[!] The file {file_path} has been securely deleted!{RESET}')
 
             except (PermissionError, ValueError, KeyboardInterrupt) as err:
@@ -43,16 +50,17 @@ def secure_delete(file_path: str, strong: int = 1) -> None:
     else:
         print(f'{RED}[?] The provided path is not a file or does not exist!{RESET}')
 
-
 def main() -> None:
-    """Entry point of the script"""
-    banner = art.text2art('secure delete')
-    ascii_banner = f'{RED}{banner}{GREEN}\t\t\t\twrite by @github.com/nhman-python{RESET}\n'
+    """
+    Entry point of the script
+    """
+    banner = art.text2art('Secure Delete')
+    ascii_banner = f'{RED}{banner}{GREEN}\t\t\t\tWritten by @github.com/nhman-python{RESET}\n'
     print(ascii_banner)
 
     parser = argparse.ArgumentParser(prog='Secure File Deletion',
                                      description='This script helps you securely delete a file '
-                                                 'by overwriting it multiple times.')
+                                                 'by overwriting it multiple times and renaming it with random names.')
     parser.add_argument('file_path',
                         type=str,
                         help='The path to the file that you want to delete securely.')
@@ -60,10 +68,9 @@ def main() -> None:
                         type=int,
                         help='The number of overwrite passes to make on the file. '
                              'Higher values increase security but take more time. '
-                             'Recommend 4.')
+                             'Recommended value is 4.')
     args = parser.parse_args()
     secure_delete(args.file_path, args.strong)
-
 
 if __name__ == '__main__':
     main()
